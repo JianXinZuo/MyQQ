261102ec69ae7198b4161ebf9639bc6241993997
Strawberry <zjx@stbchain.com>
2019-03-12 18:00:13 +0800

    修改绑定的地址

--- WalletAPI/WalletAPI/Program.cs
+++ WalletAPI/WalletAPI/Program.cs
@@ -22,6 +22,7 @@ namespace WalletAPI
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://0.0.0.0:5000")
                 .UseStartup<Startup>();
     }
 }
